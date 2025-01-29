93cdb8e373fa54470ef03fee74b7909adc4a2caf
Nahuel De La Cruz <nahuel.delacruz@outlook.com>
2025-01-29 20:32:49 -0300

    Implementación de variables de entorno, configuración de docker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const userRoutes = require('./routes/user.routes');
+const db = require('./config/db');
 
 const app = express();
 const port = 3000;
 
 app.use('/auth',userRoutes);
+
+db.authenticate()
+  .then(()=>{
+    console.log('DB connection was succeful');
+  }).catch(()=>{
+    console.log(e);
+  })
 
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
---
+++ src/config/db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize")
+
+const db = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql',
+    define: {
+      timestams: true
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
+);
+
+module.exports = db;
